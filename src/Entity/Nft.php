fd7a8e6b91c4cdec56b5ceb303bc879dc3a206eb
Nouster <djebali.mohamed88@gmail.com>
2023-05-14 14:49:17 +0200

    data table for nft, new migration

---
+++ src/Entity/Nft.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NftRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NftRepository::class)]
+class Nft
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $img = null;
+
+    #[ORM\Column]
+    private ?int $stock = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $launchDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): self
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function getLaunchDate(): ?\DateTimeInterface
+    {
+        return $this->launchDate;
+    }
+
+    public function setLaunchDate(\DateTimeInterface $launchDate): self
+    {
+        $this->launchDate = $launchDate;
+
+        return $this;
+    }
+}
